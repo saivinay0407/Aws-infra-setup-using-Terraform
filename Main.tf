d6210cde3ca7655618d140ac159ec0f833b659e7
saivinay0407 <72090530+saivinay0407@users.noreply.github.com>
2026-01-03 18:54:33 +0530

    Update Main.tf
    modifying the terraform files

--- Main.tf
+++ Main.tf
@@ -31,14 +31,14 @@ resource "aws_security_group" "Project-SG" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-001f2488b35ca8aad"
-  instance_type          = "t2.large"
-  key_name               = "Kastro"
+  ami                    = "ami-0fa91bc90632c73c9"
+  instance_type          = "t3.large"
+  key_name               = "aws_login"
   vpc_security_group_ids = [aws_security_group.Project-SG.id]
   user_data              = templatefile("./resource.sh", {})
 
   tags = {
-    Name = "Kastro"
+    Name = "Swiggyapp"
   }
   root_block_device {
     volume_size = 30
